71818c43a38e231a703826a8f5f80ebab74b8abf
SHARV <sharvofficial2006@gmail.com>
2026-04-13 09:44:03 +0530

    deployed

--- server.js
+++ server.js
@@ -11,7 +11,11 @@ const app = express();
 connectDB();
 
 // Global Middleware
-app.use(cors());
+app.use(cors({
+    origin: ['http://localhost:5173', 'https://electoral-watch-dashboard.onrender.com'], // Replace with your exact frontend Render URL
+    credentials: true 
+}));
+
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true })); 
 
